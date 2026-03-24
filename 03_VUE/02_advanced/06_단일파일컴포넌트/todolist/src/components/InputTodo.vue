327b07af2589a469a27a1e901a90d3d0f5be7a57
hyeonjin6530 <cola1876@naver.com>
2026-03-24 15:37:04 +0900

    0324_심화 실습

---
+++ 03_VUE/02_advanced/06_단일파일컴포넌트/todolist/src/components/InputTodo.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <input
+      type="text"
+      v-model.trim="todo"
+      @keyup.ente.exact="addTodo"
+      @keyup.ctrl.enter.exact="addTodo"
+    />
+    <button @click="addTodo">추가</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const todo = ref('');
+
+const addTodo = (e) => {
+  if (todo === '') return;
+
+  emit('add', {
+    id: new Date().getTime(),
+    todo: todo.value,
+    completed: e.ctrlKey ? true : false,
+  });
+
+  todo.value = '';
+};
+
+const emit = defineEmits(['add']);
+</script>
+
+<style lang="scss" scoped></style>
---
+++ 03_VUE/02_advanced/06_단일파일컴포넌트/todolist/src/components/TodoListItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <li @click="toggleCompleted(todoitem.id)">
+    <span
+      :style="{ textDecoration: todoitem.completed ? 'line-through' : 'none' }"
+      >{{ todoitem.todo }} {{ todoitem.completed ? '[완료]' : '' }}</span
+    >
+    <!-- 삭제 하기 -->
+    <button @click="deleteTodo(todoitem.id)">삭제</button>
+  </li>
+</template>
+
+<script setup>
+const props = defineProps({
+  todoitem: Object,
+});
+
+// 이벤트 전달하기 위해 emit 사용
+const emit = defineEmits(['toggle', 'delete']);
+
+const toggleCompleted = (id) => {
+  emit('toggle', id);
+};
+
+const deleteTodo = (id) => {
+  emit('delete', id);
+};
+</script>
+
+<style lang="scss" scoped></style>
