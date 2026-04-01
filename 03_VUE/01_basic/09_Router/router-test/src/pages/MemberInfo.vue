841d141703c7958ec0b21d4a03c54b94cceec0dd
hyeonjin6530 <cola1876@naver.com>
2026-04-01 15:17:03 +0900

    0401_실습

---
+++ 03_VUE/01_basic/09_Router/router-test/src/pages/MemberInfo.vue
@@ -0,0 +1,48 @@
+<template>
+  <h1>MemberInfo</h1>
+
+  <!-- <div>
+    경로패턴 : member/:id <br />
+    요청경로 : {{ route.path }} <br />
+    전달된 id : {{ route.params.id }}
+  </div> -->
+
+  <div>
+    <img :src="member.photo" alt="member.name" />
+    <h4>{{ member.name }}({{ member.role }})</h4>
+    <p>{{ member.desc }}</p>
+
+    <a
+      v-if="member.insta && member.insta !== ''"
+      class="fa fa-instagram m-1"
+      :href="member.insta"
+    ></a>
+    <a
+      v-if="member.facebook && member.facebook !== ''"
+      class="fa fa-facebook m-1"
+      :href="member.facebook"
+    ></a>
+    <a
+      v-if="member.youtube && member.youtube !== ''"
+      class="fa fa-youtube m-1"
+      :href="member.youtube"
+    ></a>
+
+    <br />
+
+    <RouterLink to="/members">멤버목록으로</RouterLink>
+  </div>
+</template>
+
+<script setup>
+import { useRoute } from 'vue-router';
+import members from '@/members.json';
+
+const route = useRoute();
+
+const memberId = Number(route.params.id);
+
+const member = members.find((m) => m.id === memberId);
+</script>
+
+<style scoped></style>
